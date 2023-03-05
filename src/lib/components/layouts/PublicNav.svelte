9f1080ee9a659421a425e9dd28e10d24c0ad5625
John Faucett <jwaterfaucett@gmail.com>
2023-03-05 12:51:41 +0100

    Add styles for landing page

--- src/lib/components/layouts/PublicNav.svelte
+++ src/lib/components/layouts/PublicNav.svelte
@@ -4,18 +4,24 @@
 	import { _ } from "svelte-i18n";
 </script>
 
-<div class="fixed w-full">
-	<div class="">
-		<a class="text-xl p-2 bg-white hover:bg-white rounded-full mx-8" href="/">
-			<img
-				src="https://raw.githubusercontent.com/quantu-app/design-platform/master/branding/logo/1x/logo-matrix-symmetric.png"
-				alt="[Q]"
-				width="34"
-				height="34"
-			/>
-		</a>
+<header aria-label="Site Header" class="bg-black">
+	<div class="mx-auto max-w-screen-lg px-2 sm:px-4 lg:px-6">
+		<div class="flex h-16 items-center justify-between">
+			<div class="flex flex-row">
+				<a class="flex text-xl p-2 bg-white hover:bg-white rounded-full mr-2" href="/">
+					<img
+						src="https://raw.githubusercontent.com/quantu-app/design-platform/master/branding/logo/1x/logo-matrix-symmetric.png"
+						alt="[Q]"
+						width="34"
+						height="34"
+					/>
+				</a>
+				<div class="text-2xl text-white align-middle mt-2 font-bold">Quantu</div>
+			</div>
+			<div class="">
+				<a href="https://quantu-app.github.io/quantu-ui" 
+					class="btn btn-black">{$_('layouts.nav.get_started')}</a>
+			</div>
+		</div>
 	</div>
-	<div class="">
-		<a class="btn btn-black btn-compact">{$_('layouts.nav.get_started')}</a>
-	</div>
-</div>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,108 +5,104 @@
 	import { goto } from '$app/navigation';
 	import { browser } from '$app/environment';
 
-	let pageTitle: string = 'QUANTU | Where learning meets fun';
+	import { _ } from "svelte-i18n";
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>{$_("home.page_title")}</title>
 </svelte:head>
 
 <PublicLayout>
 	<div class="bg-black">
 		<!-- Jumbotron -->
-	<div class="container mx-auto text-white">
-		<div class="grid sm:grid-cols-2 sm:grid-flow-row gap-2 p-10">
-			<div>
-				<h1 class="text-5xl">Master Mathematics.</h1>
-				<p class="py-6 text-2xl">
-					Learn advanced mathematics using the best methods proven by learning science.
-				</p>
-				<button type="button" class="btn btn-primary bg-blue-500 hover:bg-blue-600">
-								Get Started
-							</button>
-			</div>
-			<div>
-				<img
-						src="https://github.com/quantu-app/design-platform/raw/master/app/resources/homepage/vitruve-man2-davinci.png"
-						alt="Vitruve Man by Davinci"
-					/>
+		<div class="container mx-auto text-white max-w-screen-lg">
+			<div class="grid sm:grid-cols-2 sm:grid-flow-row gap-2 px-2 sm:px-4 lg:px-6 py-24">
+				<div>
+					<h1 class="text-5xl">{$_("home.jumbotron.title")}</h1>
+					<p class="py-6 text-2xl">
+						{$_("home.jumbotron.callout")}
+					</p>
+					<button type="button" class="btn btn-primary ">
+									{$_("home.download_btn")}
+								</button>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-	<div class="container">
-		<div class="row py-4 my-4">
-			<div class="col-12 my-5">
-				<h2 class="display-5 fw-bold text-center">Advanced Math Made Simple</h2>
-			</div>
-			<div class="col-md-4 offset-md-2 text-center">
-				<img
-					class="img-fluid mx-auto d-block "
-					src="https://github.com/quantu-app/design-platform/raw/master/app/resources/homepage/hand-crafted.png"
-					alt="Hand Crafted"
-					width="50%"
-				/>
-				<h2 class="fw-bold mt-5">Hand-crafted to build mastery</h2>
-				<p class="mb-5">
-					Quantu uses state-of-the-art research in learning science to craft lessons that stick deep
-					in your memory.
-				</p>
-			</div>
-			<div class="col-md-4 text-center">
-				<img
-					class="img-fluid mx-auto d-block "
-					src="https://github.com/quantu-app/design-platform/raw/master/app/resources/homepage/for-learners.png"
-					alt="For Learners"
-					width="50%"
-				/>
-				<h2 class="fw-bold mt-5">For learners by learners</h2>
-				<p class="mb-5">
-					Our community of volunteers design and develop lessons that make it easier for you to
-					understand complex topics.
-				</p>
-			</div>
-			<hr />
-			<div class="col-12 pb-5 my-5">
-				<h2 class="display-5 fw-bold">Build Expertise Easily</h2>
-			</div>
-			<div class="col-md-6 px-4 text-center">
-				<img
-					src="https://github.com/quantu-app/design-platform/raw/master/app/resources/homepage/decision_tree.png"
-					alt="Decision Tree"
-				/>
-			</div>
-			<div class="col-md-6 px-4">
-				<h2 class="fw-bold">Make Math Stick</h2>
-				<p class="mb-5">Learn advanced math concepts and anchor them deep in your memory.</p>
+	<!-- Page Details -->
+	<div class="container mx-auto max-w-screen-lg">
 
-				<h2 class="fw-bold pt-4">Refine Your Skills</h2>
-				<p class="mb-5">Develop your abilities in math through guided and interactive lessons.</p>
+		<div class="flex flex-col py-4 my-4 px-2 sm:px-4 lg:px-6">
+			<!-- How its built -->
+			<div class="w-full my-5">
+				<h2 class="text-5xl font-bold">{$_('home.how_its_built.header')}</h2>
+			</div>
+			<div class="w-full grid grid-cols-2 gap-10 sm:20 lg:gap-28 my-20">
+				<div class="">
+					<img
+						src="https://github.com/quantu-app/design-platform/raw/master/app/resources/homepage/hand-crafted.png"
+						alt="Hand Crafted"
+						class="w-full sm:w-1/2"
+					/>
+					<h2 class="font-bold my-5 text-3xl">Hand-crafted to build mastery</h2>
+					<p class="mb-5 text-xl">
+						Quantu uses state-of-the-art research in learning science to craft lessons that stick deep
+						in your memory.
+					</p>
+				</div>
+				<div class="">
+					<img
+						src="https://github.com/quantu-app/design-platform/raw/master/app/resources/homepage/for-learners.png"
+						alt="For Learners"
+						class="w-full sm:w-1/2"
+					/>
+					<h2 class="font-bold my-5 text-3xl">For learners by learners</h2>
+					<p class="mb-5 text-xl">
+						Our community of volunteers design and develop lessons that make it easier for you to
+						understand complex topics.
+					</p>
+				</div>
+			</div>
+			
+			<!-- Build Expertise -->
+			<div>
+				<div class="w-full pb-5 my-5">
+					<h2 class="text-5xl font-bold">Build Expertise Easily</h2>
+				</div>
+				<div class="w-full grid grid-cols-2 gap-10 sm:20 lg:gap-28 my-20">
+					<div class="text-center">
+						<img
+							src="https://github.com/quantu-app/design-platform/raw/master/app/resources/homepage/decision_tree.png"
+							alt="Decision Tree"
+						/>
+					</div>
+					<div class="">
+						<h2 class="font-bold text-3xl mb-3">Make Math Stick</h2>
+						<p class="mb-5 text-xl">Learn advanced math concepts and anchor them deep in your memory.</p>
 
-				<h2 class="fw-bold pt-4">Learn On Your Terms</h2>
-				<p>Master any topic at your own pace, fast or slow, no deadlines, no pressure.</p>
+						<h2 class="font-bold text-3xl mb-3 pt-4">Refine Your Skills</h2>
+						<p class="mb-5 text-xl">Develop your abilities in math through guided and interactive lessons.</p>
+
+						<h2 class="font-bold text-3xl mb-3 pt-4">Learn On Your Terms</h2>
+						<p class="mb-5 text-xl">Master any topic at your own pace, fast or slow, no deadlines, no pressure.</p>
+					</div>
+				</div>
 			</div>
-		</div>
-		<hr class="my-5" />
-		<div class="row py-4 my-4">
-			<div class="col-md-6 d-flex align-items-center">
-				<img
-					src="https://github.com/quantu-app/design-platform/raw/master/app/resources/homepage/circle_and_line.png"
-					alt="Circle and Line"
-				/>
-			</div>
-			<div class="col-md-6 px-4">
-				<h2 class="fw-bold">Become a Prodigy with Quantu</h2>
-				<p class="py-3">Join and become a part of Quantu's community of lifelong-learners.</p>
-				<button
-					type="button"
-					data-bs-toggle="modal"
-					data-bs-target="#sign-in-up-modal"
-					data-signup="true"
-					class="btn btn-primary"
-				>
-					Get Started
-				</button>
+			
+			<div class="w-full grid grid-cols-1 sm:grid-cols-2 gap-10 sm:20 lg:gap-28 my-20">
+				<div class="align-items-center my-10 sm:my-0">
+					<img
+						src="https://github.com/quantu-app/design-platform/raw/master/app/resources/homepage/circle_and_line.png"
+						alt="Circle and Line"
+					/>
+				</div>
+				<div class="">
+					<h2 class="font-bold text-3xl mb-3">Become a Prodigy with Quantu</h2>
+					<p class="my-10 text-xl">Join and become a part of Quantu's community of lifelong-learners.</p>
+					<a href="/" class="btn btn-primary" target="_blank">
+						{$_('home.download_btn')}
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
